c8a144fb6780f9149dcc63b19b81b234dc504f19
Dmitry Vasilyev <d.vasilyev@postgrespro.ru>
2015-10-07 16:06:45 +0300

    update nsis installer

--- build/nsis_installer.cmd
+++ build/nsis_installer.cmd
@@ -3,15 +3,15 @@ REM 1. NSIS
 REM 2. UsrMgr Plugin for NSIS http://nsis.sourceforge.net/UserMgr_plug-in
 REM 3. AccessControl Plugin for NSIS http://nsis.sourceforge.net/AccessControl_plug-in
 REM 4. Copy AddToPath.dll to %programfiles(x86)%\NSIS\Plugins
-REM 4. Visual Studio 2010 Redistibutable (x86, x64) [Place it to nsis directory]
-REM 5. PostgreSQL and PgAdmin3 binaries
+REM 5. Visual Studio 2010 Redistibutable (x86, x64) [Place it to nsis directory]
+REM 6. PostgreSQL and PgAdmin3 binaries
 
 REM Set your NSIS installation directory
 SET NSIS_PATH="%programfiles(x86)%\NSIS"
 REM Add NSIS to your PATH
 SET PATH=%PATH%;%NSIS_PATH%
 REM Also, you need to make defines if you use x64 version of PostgreSQL
-REM You need to uncomment this line: ;!define PG_64bit
+REM You need to uncomment this line: ;!define PG_64bit in postgresql.nsi
 
 REM ----------------------------------------------------------------------------
 REM Assume, you have your PostgreSQL and PgAdmin3 build in C:\pg\distr_X86_9.4.4
@@ -23,6 +23,8 @@ REM ----------------------------------------------------------------------------
 REM Just run that script and installer will appear in the nsis directory
 
 CD c:\pg\pgwininstall\nsis || GOTO :ERROR
+IF NOT EXIST vcredist_x64.exe GOTO :ERROR
+IF NOT EXIST vcredist_x86.exe GOTO :ERROR
 makensis postgresql.nsi || GOTO :ERROR
 makensis pgadmin.nsi || GOTO :ERROR
 
