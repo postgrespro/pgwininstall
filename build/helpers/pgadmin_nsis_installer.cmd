1b23c4bd5d12808d821dbfb40ce1d182d4a4ab28
Dmitry Maslyuk <d.masluk@postgrespro.ru>
2016-06-10 20:04:04 +0300

    Define BUILD_DIR for pgadmin installer

--- build/helpers/pgadmin_nsis_installer.cmd
+++ build/helpers/pgadmin_nsis_installer.cmd
@@ -31,6 +31,7 @@ SET ADMIN_INS_SOURCE_DIR="%BUILD_DIR%\distr_%ARCH%_%PG_DEF_VERSION%\pgadmin\*.*"
 >>%NSIS_RES_DIR%\pgadmin.def.nsh ECHO !define ADMIN_INS_SUFFIX %ADMIN_INS_SUFFIX%
 >>%NSIS_RES_DIR%\pgadmin.def.nsh ECHO !define ADMIN_INS_SOURCE_DIR %ADMIN_INS_SOURCE_DIR%
 >>%NSIS_RES_DIR%\pgadmin.def.nsh ECHO !define REDIST_YEAR %REDIST_YEAR%
+>>%NSIS_RES_DIR%\pgadmin.def.nsh ECHO !define BUILD_DIR %BUILD_DIR%
 IF "%ARCH%" == "X64" (
   >>%NSIS_RES_DIR%\pgadmin.def.nsh ECHO !define Admin64
 )
