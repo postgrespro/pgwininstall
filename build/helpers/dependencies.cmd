1214e5b3b4b3035cb575f2555557aa84ca5df647
Dmitry Maslyuk <d.masluk@postgrespro.ru>
2016-07-13 14:08:24 +0300

    Change url for wineditline

--- build/helpers/dependencies.cmd
+++ build/helpers/dependencies.cmd
@@ -33,7 +33,8 @@ cp -va *.exe %DEPENDENCIES_BIN_DIR%\less
 :BUILD_WINLIBEDIT
 TITLE Build winlibedit
 CD /D %DOWNLOADS_DIR%
-wget --no-check-certificate -c http://downloads.sourceforge.net/project/mingweditline/wineditline-%EDITLINE_VER%.zip
+REM wget --no-check-certificate -c http://downloads.sourceforge.net/project/mingweditline/wineditline-%EDITLINE_VER%.zip
+wget --no-check-certificate -c http://repo.postgrespro.ru/depends/wineditline-%EDITLINE_VER%.zip
 CD /D %DEPENDENCIES_SRC_DIR%
 7z x %DOWNLOADS_DIR%\wineditline-%EDITLINE_VER%.zip
 CD /D wineditline-%EDITLINE_VER%\src
