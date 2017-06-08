058cfb66a5c3cb2425214e30917da950a392a429
Dmitry Maslyuk <d.masluk@postgrespro.ru>
2017-06-08 15:40:05 +0300

    Update dependencies.cmd

--- build/helpers/dependencies.cmd
+++ build/helpers/dependencies.cmd
@@ -10,14 +10,21 @@ rm -rf %DEPENDENCIES_SRC_DIR%
 MKDIR %DEPENDENCIES_SRC_DIR%
 MKDIR %DOWNLOADS_DIR%
 
+REM TO-DO: overwrite to build rules
+:DOWNLOAD_MSYS_UTILS
+TITLE Download msys utils...
+CD /D %DOWNLOADS_DIR%
+wget --no-check-certificate -c http://repo.l.postgrespro.ru/depends/mingw_min/min_msys_X86.zip -O min_msys_%ARCH%.zip
+
 :BUILD_ICONV
 TITLE Building iconv...
 CD /D %DOWNLOADS_DIR%
-wget --no-check-certificate -c http://ftp.gnu.org/gnu/libiconv/libiconv-%ICONV_VER%.tar.gz -O libiconv-%ICONV_VER%.tar.gz
+REM wget --no-check-certificate -c http://ftp.gnu.org/gnu/libiconv/libiconv-%ICONV_VER%.tar.gz -O libiconv-%ICONV_VER%.tar.gz
+wget --no-check-certificate -c http://repo.l.postgrespro.ru/depends/libiconv-%ICONV_VER%.tar.gz -O libiconv-%ICONV_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\iconv %DEPENDENCIES_SRC_DIR%\libiconv-*
 MKDIR %DEPENDENCIES_BIN_DIR%\iconv
-tar xf libiconv-%ICONV_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
-CD /D  %DEPENDENCIES_SRC_DIR%\libiconv-%ICONV_VER%*
+tar xf libiconv-%ICONV_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
+CD /D %DEPENDENCIES_SRC_DIR%\libiconv-%ICONV_VER%*
 cp -v %ROOT%/patches/libiconv/libiconv-%ICONV_VER%-%SDK%.patch libiconv.patch
 patch -f -p0 < libiconv.patch || GOTO :ERROR
 IF %ARCH% == X64 msbuild libiconv.vcxproj /m /p:Configuration=Release /p:Platform=x64 /p:PlatformToolset=%PlatformToolset% || GOTO :ERROR
@@ -41,8 +48,9 @@ CD /D %DOWNLOADS_DIR%
 wget -c http://zlib.net/zlib-%ZLIB_VER%.tar.gz -O zlib-%ZLIB_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\zlib %DEPENDENCIES_SRC_DIR%\zlib*
 MKDIR %DEPENDENCIES_BIN_DIR%\zlib
-tar xf zlib-%ZLIB_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
+tar xf zlib-%ZLIB_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
 CD /D %DEPENDENCIES_SRC_DIR%\zlib*
+set CL=/MP
 nmake -f win32/Makefile.msc || GOTO :ERROR
 MKDIR %DEPENDENCIES_BIN_DIR%\zlib\lib %DEPENDENCIES_BIN_DIR%\zlib\include
 cp -v *.lib %DEPENDENCIES_BIN_DIR%\zlib\lib || GOTO :ERROR
@@ -79,17 +87,21 @@ CD /D %DOWNLOADS_DIR%
 TITLE Building xml...
 CD /D %DOWNLOADS_DIR%
 wget -c ftp://xmlsoft.org/libxml2/libxml2-%XML_VER%.tar.gz -O libxml2-%XML_VER%.tar.gz
-rm -rf %DEPENDENCIES_BIN_DIR%\libxml2 %DEPENDENCIES_SRC_DIR%\libxml2-
+rm -rf %DEPENDENCIES_BIN_DIR%\libxml2 %DEPENDENCIES_SRC_DIR%\libxml2-*
 MKDIR %DEPENDENCIES_BIN_DIR%\libxml2
-tar xf libxml2-%XML_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
+tar xf libxml2-%XML_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
+CD /D %DEPENDENCIES_SRC_DIR%\libxml2-*
+patch -f -p1 < %ROOT%/patches/libxml2/libxml2.patch || GOTO :ERROR
 CD /D %DEPENDENCIES_SRC_DIR%\libxml2-*\win32
 cscript configure.js compiler=msvc include=%DEPENDENCIES_BIN_DIR%\iconv\include lib=%DEPENDENCIES_BIN_DIR%\iconv\lib
 sed -i /NOWIN98/d Makefile.msvc
+set CL=/MP
 nmake /f Makefile.msvc || GOTO :ERROR
 nmake /f Makefile.msvc install || GOTO :ERROR
 cp -av bin %DEPENDENCIES_BIN_DIR%\libxml2 || GOTO :ERROR
 cp -av lib %DEPENDENCIES_BIN_DIR%\libxml2 || GOTO :ERROR
-cp -av include %DEPENDENCIES_BIN_DIR%\libxml2 || GOTO :ERROR
+MKDIR %DEPENDENCIES_BIN_DIR%\libxml2\include || GOTO :ERROR
+cp -av include\libxml2\libxml %DEPENDENCIES_BIN_DIR%\libxml2\include || GOTO :ERROR
 CD /D %DOWNLOADS_DIR%
 7z a -r %DOWNLOADS_DIR%\%DEPS_ZIP% %DEPENDENCIES_BIN_DIR%\libxml2 -y
 
@@ -100,10 +112,11 @@ CD /D %DOWNLOADS_DIR%
 wget -c ftp://xmlsoft.org/libxslt/libxslt-%XSLT_VER%.tar.gz -O libxslt-%XSLT_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\libxslt %DEPENDENCIES_SRC_DIR%\libxslt-*
 MKDIR %DEPENDENCIES_BIN_DIR%\libxslt
-tar xf libxslt-%XSLT_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
+tar xf libxslt-%XSLT_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
 CD /D %DEPENDENCIES_SRC_DIR%\libxslt-*\win32
 cscript configure.js compiler=msvc zlib=yes iconv=yes include=%DEPENDENCIES_BIN_DIR%\iconv\include;%DEPENDENCIES_BIN_DIR%\libxml2\include;%DEPENDENCIES_BIN_DIR%\zlib\include lib=%DEPENDENCIES_BIN_DIR%\iconv\lib;%DEPENDENCIES_BIN_DIR%\libxml2\lib;%DEPENDENCIES_BIN_DIR%\zlib\lib
 sed -i /NOWIN98/d Makefile.msvc
+set CL=/MP
 nmake /f Makefile.msvc || GOTO :ERROR
 nmake /f Makefile.msvc install || GOTO :ERROR
 cp -av bin %DEPENDENCIES_BIN_DIR%\libxslt || GOTO :ERROR
@@ -119,12 +132,13 @@ CD /D %DOWNLOADS_DIR%
 wget --no-check-certificate -c https://www.openssl.org/source/openssl-%OPENSSL_VER%.tar.gz -O openssl-%OPENSSL_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\openssl %DEPENDENCIES_SRC_DIR%\openssl-*
 MKDIR %DEPENDENCIES_BIN_DIR%\openssl
-tar zxf openssl-%OPENSSL_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR%
+tar zxf openssl-%OPENSSL_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR%
 CD /D %DEPENDENCIES_SRC_DIR%\openssl-*
 IF %ARCH% == X86 perl Configure VC-WIN32 no-asm   || GOTO :ERROR
 IF %ARCH% == X64 perl Configure VC-WIN64A no-asm  || GOTO :ERROR
 IF %ARCH% == X86 call ms\do_ms
 IF %ARCH% == X64 call ms\do_win64a.bat
+set CL=/MP
 nmake -f ms\ntdll.mak || GOTO :ERROR
 MKDIR %DEPENDENCIES_BIN_DIR%\openssl\lib
 MKDIR %DEPENDENCIES_BIN_DIR%\openssl\include
@@ -141,10 +155,11 @@ CD /D %DOWNLOADS_DIR%
 :BUILD_GETTEXT
 TITLE Building gettext...
 CD /D %DOWNLOADS_DIR%
-wget --no-check-certificate -c http://ftp.gnu.org/gnu/gettext/gettext-%GETTEXT_VER%.tar.gz -O gettext-%GETTEXT_VER%.tar.gz
+REM wget --no-check-certificate -c http://ftp.gnu.org/gnu/gettext/gettext-%GETTEXT_VER%.tar.gz -O gettext-%GETTEXT_VER%.tar.gz
+wget --no-check-certificate -c http://repo.l.postgrespro.ru/depends/gettext-%GETTEXT_VER%.tar.gz -O gettext-%GETTEXT_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\libintl %DEPENDENCIES_SRC_DIR%\gettext-*
 MKDIR %DEPENDENCIES_BIN_DIR%\libintl
-tar xf gettext-%GETTEXT_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
+tar xf gettext-%GETTEXT_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
 CD /D %DEPENDENCIES_SRC_DIR%\gettext-*
 cp -v %ROOT%/patches/gettext/gettext-%GETTEXT_VER%-%SDK%.patch gettext.patch
 patch -f -p0 < gettext.patch || GOTO :ERROR
@@ -166,7 +181,7 @@ CD /D %DOWNLOADS_DIR%
 wget --no-check-certificate -c http://www.libssh2.org/download/libssh2-%LIBSSH2_VER%.tar.gz -O libssh2-%LIBSSH2_VER%.tar.gz
 rm -rf %DEPENDENCIES_BIN_DIR%\libssh2 %DEPENDENCIES_SRC_DIR%/libssh2-*
 MKDIR %DEPENDENCIES_BIN_DIR%\libssh2
-tar xf libssh2-%LIBSSH2_VER%.tar.gz -C %DEPENDENCIES_SRC_DIR% || GOTO :ERROR
+tar xf libssh2-%LIBSSH2_VER%.tar.gz -C %DEPENDENCIES_SRC_UDIR% || GOTO :ERROR
 cp -va %DEPENDENCIES_SRC_DIR%/libssh2-*/include %DEPENDENCIES_BIN_DIR%\libssh2\include  || GOTO :ERROR
 cp -va %DEPENDENCIES_SRC_DIR%/libssh2-*/win32/libssh2_config.h %DEPENDENCIES_BIN_DIR%\libssh2\include  || GOTO :ERROR
 CD /D %DOWNLOADS_DIR%
@@ -200,9 +215,7 @@ GOTO :DONE
 
 :ERROR
 ECHO Failed with error #%errorlevel%.
-REM PAUSE
 EXIT /b %errorlevel%
 
 :DONE
 ECHO Done.
-REM PAUSE
