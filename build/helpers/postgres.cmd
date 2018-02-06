b9ce9eecebf9b7ff227f3210a0adc65baeeb16da
Dmitry Maslyuk <d.masluk@postgrespro.ru>
2018-02-06 18:52:10 +0300

    PGPRO-1256: remove test_* extensions from installer

--- build/helpers/postgres.cmd
+++ build/helpers/postgres.cmd
@@ -99,6 +99,11 @@ rem We need ICONV and LibIntl DLLS available during install for ZIC to work
 rem no need to copy them, just add to PATH
 PATH %PATH%;%DEPENDENCIES_BIN_DIR%\libintl\lib;%DEPENDENCIES_BIN_DIR%\iconv\lib
 %PERL_EXE% install.pl %BUILD_DIR%\distr_%ARCH%_%PGVER%\postgresql || GOTO :ERROR
+
+REM remove test_* extensions after install
+rm -rf %BUILD_DIR%\distr_%ARCH%_%PGVER%\postgresql\share\extension\test_* || GOTO :ERROR
+rm -rf %BUILD_DIR%\distr_%ARCH%_%PGVER%\postgresql\lib\test_* || GOTO :ERROR
+
 rem now actually copy DLLs of dependencies into our bindir
 cp -va %DEPENDENCIES_BIN_DIR%/libintl/lib/*.dll    %BUILD_DIR%\distr_%ARCH%_%PGVER%\postgresql\bin || GOTO :ERROR
 cp -va %DEPENDENCIES_BIN_DIR%/iconv/lib/*.dll      %BUILD_DIR%\distr_%ARCH%_%PGVER%\postgresql\bin || GOTO :ERROR
