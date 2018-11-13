78270a1a4725d648fc65d8d710469fcec237625f
Victor Wagner <vitus@wagner.pp.ru>
2018-11-13 18:47:43 +0300

    Fix filelist generation to remove unpackaged files

--- build/helpers/postgres_nsis_installer.cmd
+++ build/helpers/postgres_nsis_installer.cmd
@@ -122,17 +122,28 @@ GOTO :ENDLIC
 
 CD /D %NSIS_RES_DIR% || GOTO :ERROR
 rem Genarate file lists
-
-type server.files > allserver.files
+rem Remove old filelists first
+rm -f *_list.nsi
+rem tune pattern lists to major version and product
+IF "%PG_MAJOR_VERSION%" == "9.6" (
+	sed "s/wal/xlog/" server.files > allserver.files
+	sed  "s/wal/xlog/" client.files > allclient.files
+	echo ./bin/createlang.* >> allclient.files
+	echo ./bin/droplang.* >> allclient.files
+) ELSE (
+    cat server.files > allserver.files
+	type client.files > allclient.files
+)
 IF NOT "%PG_MAJOR_VERSION%" == "9.6" GOTO :NO_PGPRO_UPGRADE
 IF "%PRODUCT_NAME%" == "PostgresPro" GOTO :ADD_PGPRO_UPGRADE
 IF "%PRODUCT_NAME%" == "PostgresProEnterprise" GOTO :ADD_PGPRO_UPGRADE
 GOTO :NO_PGPRO_UPGRADE
 :ADD_PGPRO_UPGRADE
-type server.files pgpro_upgrade.files > allserver.files
+type pgpro_upgrade.files >> allserver.files
 :NO_PGPRO_UPGRADE
 
-%PYTHON64_PATH%/python %ROOT%/build/helpers/genlists.py %PG_INS_SOURCE_DIR% client.files devel.files plperl.files plpython2.files plpython3.files unneeded.files allserver.files || GOTO :ERROR
+rem expand pattern lists to actual file lists
+%PYTHON64_PATH%/python %ROOT%/build/helpers/genlists.py %PG_INS_SOURCE_DIR% allclient.files devel.files plperl.files plpython2.files plpython3.files unneeded.files allserver.files || GOTO :ERROR
 
 rem generate installer itself
 makensis postgresql.nsi || GOTO :ERROR
