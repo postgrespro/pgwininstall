c878f10400585c350da58ad2ea23393bb0fc4210
Grigory Smolkin <g.smolkin@postgrespro.ru>
2019-08-08 22:04:09 +0300

    WIP

--- build/helpers/probackup_installer.cmd
+++ build/helpers/probackup_installer.cmd
@@ -1,31 +1,13 @@
 echo.
-echo Building Installer...
+echo Building PG_PROBACKUP Installer...
 
-
->Files.wxs  ECHO ^<^?xml version="1.0" encoding="UTF-8"?^>
->>Files.wxs  ECHO ^<Wix xmlns="http://schemas.microsoft.com/wix/2006/wi"^>
->>Files.wxs  ECHO ^<^?include Variables.wxi?^>
->>Files.wxs  ECHO ^<Fragment^>
->>Files.wxs  ECHO ^<DirectoryRef Id = "INSTALLLOCATION"^>
->>Files.wxs  ECHO 	^<^Directory Id="BIN" Name="bin"^>
->>Files.wxs  ECHO     		^<^Component Id="ProductComponent" Guid="$(var.ComponentCode)" Win64="$(var.Win64)"^>
-perl genfilelist.pl ./out/bin/*.* Files.wxs
->>Files.wxs  ECHO 		^<^/Component^>
->>Files.wxs  ECHO 	^<^/Directory^>
->>Files.wxs  ECHO ^<^/DirectoryRef^>
->>Files.wxs  ECHO ^<^/Fragment^>
->>Files.wxs  ECHO ^<^/Wix^>
-
+SET PRODUCT_NAME=PostgreSQL
+SET BIN_DIR=%BUILD_DIR%\pg_probackup_%PG_MAJOR_VERSION%_%PROBACKUP_VERSION%_%ARCH%
 SET WIXDIR=C:\Program Files (x86)\WiX Toolset v3.11\bin
-perl regenguids.pl Variables.wxi
-move Variables.wxi.out Variables.wxi
-echo on
-"%WIXDIR%\candle" -nologo  -dAPPVERSION="%APPVERSION%" -dPG_REG_KEY="%PG_REG_KEY%" -dPG_DEF_BRANDING="%PG_DEF_BRANDING%" Product.wxs Files.wxs || goto :ERROR
-                                                                                                      
-rem SET INS_FILE=pg-probackup-%EDITION%-%PG_MAJOR_VERSION%-%APPVERSION%-%BITS%.msi
-SET INS_FILE=pg-probackup-%EDITION%-%PG_MAJOR_VERSION%-%APPVERSION%.msi
+SET PG_DEF_BRANDING=PostgreSQL%PG_MAJOR_VERSION%
+SET PG_REG_KEY=SOFTWARE\Postgres Professional\%ARCH%\%PRODUCT_NAME%\%PG_MAJOR_VERSION%\Installations\postgresql-%PG_MAJOR_VERSION%
 
-"%WIXDIR%\light" -sice:ICE03 -sice:ICE25 -sice:ICE82 -sw1101 -nologo -ext WixUIExtension -cultures:ru-ru -o %INS_FILE% Files.wixobj Product.wixobj || goto :ERROR
+CD /D %ROOT%\wix\pg_probackup || GOTO :ERROR
 
 echo.
 echo Building Full Installer...
@@ -36,25 +18,18 @@ echo Building Full Installer...
 >>Files.wxs  ECHO ^<Fragment^>
 >>Files.wxs  ECHO ^<DirectoryRef Id = "INSTALLLOCATION"^>
 >>Files.wxs  ECHO     		^<^Component Id="ProductComponent" Guid="$(var.ComponentCode)" Win64="$(var.Win64)"^>
-perl genfilelist.pl ./out_full/*.* Files.wxs
+perl genfilelist.pl %BIN_DIR%/*.* Files.wxs
 >>Files.wxs  ECHO 		^<^/Component^>
 >>Files.wxs  ECHO ^<^/DirectoryRef^>
 >>Files.wxs  ECHO ^<^/Fragment^>
 >>Files.wxs  ECHO ^<^/Wix^>
-"%WIXDIR%\candle" -nologo  -dAPPVERSION="%APPVERSION%" -dPG_REG_KEY="%PG_REG_KEY%" -dPG_DEF_BRANDING="%PG_DEF_BRANDING%" Product_separate.wxs Files.wxs || goto :ERROR
+"%WIXDIR%\candle" -nologo  -dAPPVERSION="%PROBACKUP_VERSION%" -dPG_REG_KEY="%PG_REG_KEY%" -dPG_DEF_BRANDING="%PG_DEF_BRANDING%" Product_separate.wxs Files.wxs || goto :ERROR
                                                                                                       
-rem SET INS_FILE=pg-probackup-%EDITION%-%PG_MAJOR_VERSION%-%APPVERSION%-%BITS%-standalone.msi
-SET INS_FILE=pg-probackup-%EDITION%-%PG_MAJOR_VERSION%-%APPVERSION%-standalone-en.msi
+SET INS_FILE=pg-probackup-%PROBACKUP_EDITION%-%PG_MAJOR_VERSION%-%PROBACKUP_VERSION%-standalone-en.msi
 
 "%WIXDIR%\light" -sice:ICE03 -sice:ICE25 -sice:ICE82 -sw1101 -nologo -ext WixUIExtension -cultures:en-us -o %INS_FILE% Files.wixobj Product_separate.wixobj || goto :ERROR
 
-SET INS_FILE=pg-probackup-%EDITION%-%PG_MAJOR_VERSION%-%APPVERSION%-standalone-ru.msi
-
-"%WIXDIR%\light" -sice:ICE03 -sice:ICE25 -sice:ICE82 -sw1101 -nologo -ext WixUIExtension -cultures:ru-ru -o %INS_FILE% Files.wixobj Product_separate.wixobj || goto :ERROR
-
-
 goto :DONE
-
 
 :ERROR
 ECHO Failed with error #%errorlevel%.
